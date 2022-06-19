d27db7c60bc661650680e1e7f44426ff9cd30a29
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2022-06-19 14:14:08 +0300

    Update ListMovies.jsx

--- src/components/ListMovies/ListMovies.jsx
+++ src/components/ListMovies/ListMovies.jsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import s from "./ListMovies.module.css";
 
 function ListMovies({ results }) {
+  const location = useLocation();
   return (
     <>
       <ul className={s.home}>
         {results.map(({ id, poster_path, title }) => {
           return (
             <li key={id} className={s.card}>
-              <Link to={`${id}`}>
+              <Link state={{ from: location }} to={`${id}`}>
                 <img
                   className={s.card__img}
                   src={`https://image.tmdb.org/t/p/w200${poster_path}`}
